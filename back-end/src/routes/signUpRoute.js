98bd2b3e7381748e99294b235bdc56d60718fe4e
NehaParmar <neha.parmar227@gmail.com>
2024-09-05 16:27:07 +0530

    converted the signup page to aws signup

--- back-end/src/routes/signUpRoute.js
+++ back-end/src/routes/signUpRoute.js
@@ -1,70 +1,54 @@
+import { CognitoUserAttribute } from "amazon-cognito-identity-js";
 import { getDbConnection } from "../db";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { v4 as uuid } from "uuid";
-import { sendEmail } from "../util/sendEmail";
+import { awsUserPool } from "../util/awsUserPool";
 
 export const signUpRoute = {
   path: "/api/signup",
   method: "post",
   handler: async (req, res) => {
     const { email, password } = req.body;
-    const db = getDbConnection("react-auth-db");
-    const user = await db.collection("users").findOne({ email });
-    if (user) res.sendStatus(409);
+    const attributes = [
+      new CognitoUserAttribute({ Name: "email", Value: email }),
+    ];
 
-    const passwordHash = await bcrypt.hash(password, 10);
-    const verificationString = uuid();
-    const startingInfo = {
-      hairColor: "",
-      favouriteFood: "",
-      bio: "",
-    };
-
-    const result = await db.collection("users").insertOne({
+    awsUserPool.signUp(
       email,
-      passwordHash,
-      info: startingInfo,
-      isVerified: false,
-      verificationString,
-    });
-    console.log("---- signup result ----", result);
-    const { insertedId } = result;
-    console.log("----insertedId ----", insertedId);
-
-    try {
-      await sendEmail({
-        to: email,
-        from: "nehastestingemail@gmail.com",
-        subject: " Please verify your email",
-        text: `
-              Thanks for signing up! To verify your email, click here:
-              http://localhost:3000/verify-email/${verificationString}
-        `,
-      });
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(500);
-    }
-
-    jwt.sign(
-      {
-        id: insertedId,
-        email,
-        info: startingInfo,
-        isVerified: false,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "2d",
-      },
-      (err, token) => {
-        console.log("----error ----", err);
-        console.log("----token----", token);
+      password,
+      attributes,
+      null,
+      async (err, awsResult) => {
         if (err) {
-          return res.status(500).send(err);
+          console.log(err);
+          res.status(500).json({ message: "Unable to sign-up user." });
         }
-        res.status(200).json({ token });
+
+        const db = getDbConnection("react-auth-db");
+        const startingInfo = {
+          hairColor: "",
+          favouriteFood: "",
+          bio: "",
+        };
+
+        const result = await db
+          .collection("users")
+          .insertOne({ email, info: startingInfo });
+
+        const { insertedId } = result;
+        jwt.sign(
+          {
+            id: insertedId,
+            isVerified: false,
+            email,
+            info: startingInfo,
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: "2d" },
+          (err, token) => {
+            if (err) return res.sendStatus(500);
+            res.status(200).json({ token });
+          }
+        );
       }
     );
   },
--- front-end/src/pages/SignUpPage.js
+++ front-end/src/pages/SignUpPage.js
@@ -18,7 +18,7 @@ export const SignUpPage = () => {
     });
     const { token } = response.data;
     setToken(token);
-    history.push("/please-verify");
+    history.push(`/please-verify?${encodeURIComponent(emailValue)}`);
   };
 
   return (
